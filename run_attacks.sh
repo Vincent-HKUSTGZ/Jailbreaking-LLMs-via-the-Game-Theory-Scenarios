17e77bd0387cd1921b36c27db051071dfdec02df
Vincent-HKUSTGZ <zhensun26@gmail.com>
2025-11-20 21:16:18 -0500

    Update run_attacks.sh

--- run_attacks.sh
+++ run_attacks.sh
@@ -1,2 +1,1 @@
-python jailbreak.py --rounds 5 --output_dir results/llama3.1-405b --attack --target1 llama3.1-405b --target2 llama3.1-405b --attacker gpt-4o-mini --attack_type ours
 python jailbreak.py --rounds 5 --output_dir results/deepseek_r1 --attack --target1 deepseek_r1 --target2 deepseek_r1 --attacker gpt-4o-mini --attack_type ours
